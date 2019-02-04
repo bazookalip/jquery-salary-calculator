d6e8cb02719825524e1bf35a579dd36f38b7f44b
Marcus Anderberg <marcusanderberg@gmail.com>
2019-02-04 09:21:56 -0600

    fixed my calculation

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
-let startingCost = 15500;
+let totalCost = 15500;
+
 
 $(document).ready(onReady);
 
 function onReady(){
     $('#tableBody').on('click', '.deleteButton', deleteButton);
     $('#submit').on('click', submit); 
-    $('#totalCost').text(startingCost);
+    $('#totalCost').text(totalCost);
 } 
 
 function submit() {
@@ -14,14 +15,12 @@ function submit() {
     let id = $('#id').val();
     let title = $('#title').val();
     let salary = $('#salary').val();
-    let totalCost = startingCost + (salary / 12);
-    startingCost += totalCost;
-    totalCost = totalCost.toFixed(2);
+    totalCost += (salary / 12);
     if (totalCost >= 20000) {
     $('#total').css('background-color', 'red',);
     $('#total').css('color', 'white');
     }
-    $('#totalCost').text(totalCost);
+    $('#totalCost').text(totalCost.toFixed(2));
     $('#tableBody').append(`
          <tr>
                 <td>${firstName} </td>
